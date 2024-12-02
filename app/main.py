ae48c8775d628810d2ecf50cf93c3f7d52ec4907
F.Mio <52245444+f-mio@users.noreply.github.com>
2024-12-02 19:14:41 +0900

    main.pyからのuvicorn立ち上げ設定

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+# 標準モジュール
 import os
+# サードパーティ製モジュール
+import uvicorn
 from dotenv import load_dotenv
+from fastapi import FastAPI
 
-
+# .env記載情報をロード
 load_dotenv()
 
+# FastAPIインスタンス
+app = FastAPI()
+
+
+# [TODO] デモ
+@app.get("/")
+def demo():
+    return {"message": "Hello, FastAPI"}
+
+
+# メイン処理の場合はuvicornを立ち上げる。 ref: https://www.uvicorn.org/#running-programmatically
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=os.getenv('FAST_API_HOST'), port=8000,
+        reload=True
+    )
