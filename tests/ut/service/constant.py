a663ff11d5f0cb15d284d55e34eddbf7ac609efb
F.Mio <52245444+f-mio@users.noreply.github.com>
2024-12-03 23:39:47 +0900

    requirements.txt更新　pytest-mock追加

--- tests/ut/service/constant.py
+++ tests/ut/service/constant.py
@@ -3,11 +3,11 @@
 class HashTestConst:
 
     CORRECT_PASSWORD_LIST: list[str] = [
-        "Passw0|^D", "12345"
+        "Passw0|^D", "!23Tdsa45_"
     ]
 
-# # 現状は使用なし。今後、Weak Passを弾く仕組みをservicesに導入する可能性あり。
-# WEAK_PASSWORD_LIST: list[str] = [
-#     "123456", "abcd123",
-# ]
+    # # 現状は使用なし。今後、Weak Passを弾く仕組みをservicesに導入する可能性あり。
+    # WEAK_PASSWORD_LIST: list[str] = [
+    #     "123456", "abcd123",
+    # ]
 
