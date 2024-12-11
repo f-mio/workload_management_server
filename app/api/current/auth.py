954e3627c84810d8ddfa6b49586f1260b98f36ea
F.Mio <52245444+f-mio@users.noreply.github.com>
2024-12-11 20:11:46 +0900

    CSRF Token作成エンドポイントの修正 (signed_tokenも使用する形に修正)

--- app/api/current/auth.py
+++ app/api/current/auth.py
@@ -1,5 +1,5 @@
 # サードパーティ製モジュール
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from fastapi_csrf_protect import CsrfProtect
 # プロジェクトモジュール
 from services.auth import Auth_Utils
@@ -12,7 +12,14 @@ router = APIRouter(
 auth = Auth_Utils()
 
 @router.get("/csrftoken", response_model=CsrfType)
-async def get_csrf_token(csrf_protect: CsrfProtect = Depends()):
+async def get_csrf_token(response: Response, csrf_protect: CsrfProtect = Depends()):
     csrf_token, signed_token = csrf_protect.generate_csrf()
-    res = {"csrf_token": csrf_token}
+    response.set_cookie(
+        key="fastapi_signed_token",
+        value=signed_token,
+        httponly=True,
+        secure=True,
+        samesite="none"
+    )
+    res = { "csrf_token": csrf_token }
     return res
--- app/models/auth.py
+++ app/models/auth.py
@@ -11,8 +11,6 @@ load_dotenv()
 # csrf-protectのモデル設定
 class CsrfSettings(BaseModel):
     secret_key: str  = f"{os.getenv('SECRET_KEY_FOR_CSRF_TOKEN')}"
-    cookie_samesite: str = "none"
-    cookie_secure: bool = True
 
 
 # CSRFトークン
