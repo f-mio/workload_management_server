48b6766f29cd1e5f0bdd75e38a518bafaee05d77
F.Mio <52245444+f-mio@users.noreply.github.com>
2025-01-27 23:48:59 +0900

    登録済み工数のPydanticモデル追加

--- app/models/workloads.py
+++ app/models/workloads.py
@@ -1,4 +1,5 @@
 # 標準モジュール
+from typing import Optional
 import datetime as dt
 # サードパーティ製モジュール
 from pydantic import BaseModel
@@ -14,9 +15,38 @@ class WorkloadInfoFromDB(BaseModel):
     update_timestamp: dt.datetime
     create_timestamp: dt.datetime
 
+
 class WorkloadForm(BaseModel):
     subtask_id: int
     user_id: int
     work_date: dt.date
     workload_minute: int
     detail: str
+
+
+class WorkloadCondition(BaseModel):
+    specity_user_id: Optional[int] = None
+    target_date: Optional[dt.date] = None
+    lower_date: Optional[dt.date] = None
+    upper_date: Optional[dt.date] = None
+    project_id: Optional[int] = None
+    issue_list: Optional[list] = None
+
+
+class RegisteredWorkload(BaseModel):
+    project_id: int
+    project_name: str
+    issue_id_1: int
+    issue_name_1: str
+    issue_id_2: int
+    issue_name_2: str
+    subtask_id: int
+    subtask_name: str
+    workload_id: int
+    user_id: int
+    user_name: str
+    work_date: dt.date
+    workload_minute: int
+    detail: str
+    update_timestamp: dt.datetime
+    create_timestamp: dt.datetime
